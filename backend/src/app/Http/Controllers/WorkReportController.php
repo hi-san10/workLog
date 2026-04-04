c9082c197f4c2d4be0c0f3c50f7957dd1839f190
hi-san <h.ahkh10@icloud.com>
2026-04-05 06:21:53 +0900

    feat: 日次記録登録実装

--- backend/src/app/Http/Controllers/WorkReportController.php
+++ backend/src/app/Http/Controllers/WorkReportController.php
@@ -2,12 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Contractor;
+use App\Models\Maker;
+use App\Models\Task;
+use App\Models\WorkReport;
+use App\Models\WorkSite;
+use App\Models\WorkDetail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
 
 class WorkReportController extends Controller
 {
     public function index()
     {
-        return view('work_report');
+        $contractors = Contractor::all();
+        $work_sites = WorkSite::all();
+        $makers = Maker::all();
+        $tasks = Task::all();
+
+        return view('work_report', compact('contractors', 'work_sites', 'makers', 'tasks'));
+    }
+
+    public function store(Request $request)
+    {
+        DB::transaction(function () use(
+            $request,
+        ) {
+            $work_report = WorkReport::create([
+                'date' => $request->date,
+                'contractors_id' => $request->contractor,
+                'work_sites_id' => $request->work_site,
+            ]);
+
+            WorkDetail::create([
+                'work_reports_id' => $work_report->id,
+                'makers_id' => $request->maker,
+                'tasks_id' => $request->task,
+                'quantity' => $request->quantity,
+            ]);
+        });
+
+        return back();
     }
 }
--- backend/src/resources/views/work_report.blade.php
+++ backend/src/resources/views/work_report.blade.php
@@ -1,1 +1,34 @@
 @extends('layouts/app')
+
+@section('content')
+<div>
+    <form action="workReport" method="post">
+        @csrf
+        <label for="">日付</label>
+        <input type="date" name="date">
+        <select name="contractor" id="">
+            @foreach($contractors as $contractor)
+                <option value="{{ $contractor->id }}">{{ $contractor->name }}</option>
+            @endforeach
+        </select>
+        <select name="work_site" id="">
+            @foreach($work_sites as $work_site)
+                <option value="{{ $work_site->id }}">{{ $work_site->name }}</option>
+            @endforeach
+        </select>
+        <select name="maker" id="">
+            @foreach($makers as $maker)
+                <option value="{{ $maker->id }}">{{ $maker->name }}</option>
+            @endforeach
+        </select>
+        <select name="task" id="">
+            @foreach($tasks as $task)
+                <option value="{{ $task->id }}">{{ $task->name }}{{ $task->unit }}</option>
+            @endforeach
+        </select>
+        <label for="quantity">数量</label>
+        <input type="number" name="quantity" id="quantity">
+        <input type="submit">
+    </form>
+</div>
+@endsection
